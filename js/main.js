b27a10e2c39a6986430ff592e3c01d24a550d578
Manpreet Singh <mskhurana729@gmail.com>
2024-08-16 19:05:49 +0530

    feat: Add playGame Function
    Nested playRound and global variables inside this Function
    to decrease the risk of unintended modifications. This Function
    plays 5 rounds of game and in last show the results.
    
    Manpreet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
 
 
 // 1. Declare a getComputerChoice function
@@ -55,37 +53,76 @@ function getHumanChoice() {
 // for playRound
 //we will declare a playRound function which have 2 inputs human and computer
 
-function playRound(humanChoice, computerChoice) {
-    // then we will use conditions
-    // if human and computer both choose rock/paper/scissors then we will display that its a tie and we will not anyone's score
-    if (humanChoice === computerChoice) {
-        console.log(`You V/S Computer\n${humanChoice} VS ${computerChoice}It's a tie.\n Current Score is\n Computer Score: ${computerScore} Your Score: ${humanScore}`);
-        //if human choice is rock and computer choice is scissor or human's choice is paper and computer choice is rock or human choice is scissors and computer choice is paper we will display that human won and increment the human score
+//play game
+//we need to create a function which calls playRound 5 times and also keep track of the winner
 
-    } else if (
-        (humanChoice === "Rock" && computerChoice === "Scissors") ||
-        (humanChoice === "Paper" && computerChoice === "Rock") ||
-        (humanChoice === "Scissors" && computerChoice === "Paper")) {
-        ++humanScore;
-        console.log(`You V/S Computer\n${humanChoice} VS ${computerChoice}\nYou win.\nCurrent Score is:\nComputer Score: ${computerScore} Your Score: ${humanScore}`);
+//Declare playGame function
+//Nest playRound and computer choice and human choice variable in it.
+//Use for loop  to call human choice and computer choice and then use these choices as parameters for playRound.
+function playGame() {
 
-        //if computer choice is rock and human choice is scissor or computer's choice is paper and human choice is rock or computer choice is scissors and human choice is paper we will display that computer won and increment the computer's score
-    } else {
-        ++computerScore;
-        console.log(`You V/S Computer\n${humanChoice} VS ${computerChoice}\nComputer Wins.\nCurrent Score is:\nComputer Score: ${computerScore} Your Score: ${humanScore}`);
+    let humanScore = 0;
+    let computerScore = 0;
+
+
+    function playRound(humanChoice, computerChoice) {
+
+        // then we will use conditions
+        // if human and computer both choose rock/paper/scissors then we will display that its a tie and we will not anyone's score
+
+        if (humanChoice === computerChoice) {
+            console.log(`You V/S Computer\n${humanChoice} VS ${computerChoice}It's a tie.\n Current Score is\n Computer Score: ${computerScore} Your Score: ${humanScore}`);
+
+            //if human choice is rock and computer choice is scissor or human's choice is paper and computer choice is rock or human choice is scissors and computer choice is paper we will display that human won and increment the human score
+
+        } else if (
+            (humanChoice === "Rock" && computerChoice === "Scissors") ||
+            (humanChoice === "Paper" && computerChoice === "Rock") ||
+            (humanChoice === "Scissors" && computerChoice === "Paper")) {
+            ++humanScore;
+            console.log(`You V/S Computer\n${humanChoice} VS ${computerChoice}\nYou win.\nCurrent Score is:\nComputer Score: ${computerScore} Your Score: ${humanScore}`);
+
+            //if computer choice is rock and human choice is scissor or computer's choice is paper and human choice is rock or computer choice is scissors and human choice is paper we will display that computer won and increment the computer's score
+
+        } else {
+            ++computerScore;
+            console.log(`You V/S Computer\n${humanChoice} VS ${computerChoice}\nComputer Wins.\nCurrent Score is:\nComputer Score: ${computerScore} Your Score: ${humanScore}`);
+
+        }
+
+    }
+    function playFiveRounds() {
+        for (let i = 0; i < 5; i++) {
+
+            let humanSelection = getHumanChoice();
+            let computerSelection = getComputerChoice();
+
+            playRound(humanSelection, computerSelection);
+
+        }
+
+    }
+    function showWinner() {
+        //if human score is equal to computer score then display its a tie
+        // console.clear();
+        if (humanScore === computerScore) {
+
+            console.log("It's a Tie");
+            //If humanScore is more than computer score human is winner
+        } else if (humanScore >= computerScore) {
+
+            console.log("You Won The Series!!!!");
+            //otherwise computer won
+        } else {
+            console.log("You Lost! Computer Won The Series!");
+
+        }
 
     }
 
+    playFiveRounds();
+    showWinner();
+
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
-
-
-// function showGame() {
-//     console.log(getHumanChoice());
-//     console.log(getComputerChoice());
-// }
-
-// showGame();
+playGame();
